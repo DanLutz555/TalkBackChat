5647acc0e34cc518d3109ea3894ec0b28108f28e
DanLutz555 <138560265+DanLutz555@users.noreply.github.com>
2024-12-23 23:24:48 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,56 @@
-// server.mjs
-import { createServer } from "node:http";
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-const server = createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World!\n");
+const app = express();
+app.use(bodyParser.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://danluts:ABR7749@cluster0.cbmh7bs.mongodb.net/chat-games?retryWrites=true&w=majority&appName=user",
+    {
+  
+    }
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
-// starts a simple http server locally on port 3000
-server.listen(3000, "127.0.0.1", () => {
-  console.log("Listening on 127.0.0.1:3000");
+const User = mongoose.model("User", UserSchema);
+
+app.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({ email, password: hashedPassword });
+  await newUser.save();
+  res.status(201).send("User registered");
 });
 
-// run with `node server.mjs`
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).send("Invalid credentials");
+  }
+  const token = jwt.sign({ id: user._id }, "your_jwt_secret", {
+    expiresIn: "1h",
+  });
+  res.json({ token });
+});
+
+// הגדרת הנתיב לתיקיית CLIENT
+app.use(express.static(path.join(__dirname, "../client")));
+
+// הפניית כל הבקשות שאינן ל-API לעמוד ה-Sign In
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client","sign-in.html"));
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
